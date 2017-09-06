72382666c00ca71ecb24112c0e7a55465285e2a3
kendrahiggins <31599835+kendrahiggins@users.noreply.github.com>
2017-09-06 12:52:21 -0400

    Merged, update to new file

--- scripts/main.js
+++ scripts/main.js
@@ -4,15 +4,4 @@
     document.getElementById('display').innerHTML =
                 document.getElementById("user_input").value + ' is mine too!';
 }
-=======
-function setActive() {
-  aObj = document.getElementById('nav').getElementsByTagName('a');
-  for(i=0;i<aObj.length;i++) {
-    if(document.location.href.indexOf(aObj[i].href)>=0) {
-      aObj[i].className='active';
-    }
-  }
-}
 
-window.onload = setActive;
->>>>>>> 7a7a333a2000e42d5b5742268b4027d459f33e21

3f7ff845eb1220bc7d3310bc7ed2771c7fa25723
kendrahiggins <31599835+kendrahiggins@users.noreply.github.com>
2017-09-07 17:11:13 -0400

    Java Navigation Update

--- scripts/main.js
+++ scripts/main.js
@@ -1,18 +1,9 @@
-<<<<<<< HEAD
-
  function showInput() {
     document.getElementById('display').innerHTML =
                 document.getElementById("user_input").value + ' is mine too!';
 }
-=======
-function setActive() {
-  aObj = document.getElementById('nav').getElementsByTagName('a');
-  for(i=0;i<aObj.length;i++) {
-    if(document.location.href.indexOf(aObj[i].href)>=0) {
-      aObj[i].className='active';
-    }
-  }
-}
 
-window.onload = setActive;
->>>>>>> 7a7a333a2000e42d5b5742268b4027d459f33e21
+
+var nav = document.getElementById('nav')
+nav.addEventListener("mouseover", function ( event ){
+ event.target.style.color = "white";
